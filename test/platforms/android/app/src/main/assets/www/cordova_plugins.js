eeabc07e7c25eacb8a7f29d7db17c8e8048e46fa
walaa111 <walaagamil18@gmail.com>
2020-03-18 00:40:55 +0200

    edit

--- test/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ test/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,19 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
-    "clobbers": [
-      "OneSignal"
-    ]
-  }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-  "cordova-plugin-whitelist": "1.3.4",
-  "onesignal-cordova-plugin": "2.8.4"
-};
-// BOTTOM OF METADATA
+  module.exports = [
+    {
+      "id": "onesignal-cordova-plugin.OneSignal",
+      "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+      "pluginId": "onesignal-cordova-plugin",
+      "clobbers": [
+        "OneSignal"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "onesignal-cordova-plugin": "2.8.4"
+  };
 });
