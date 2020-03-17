e4fa7dcb761378745a970230319403ad2f45587b
walaa111 <walaagamil18@gmail.com>
2020-03-17 15:54:37 +0200

    add onesignal

--- test/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ test/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "onesignal-cordova-plugin.OneSignal",
+      "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+      "pluginId": "onesignal-cordova-plugin",
+      "clobbers": [
+        "OneSignal"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "onesignal-cordova-plugin": "2.8.4"
   };
 });
--- test/www/js/index.js
+++ test/www/js/index.js
@@ -44,3 +44,20 @@ var app = {
 };
 
 app.initialize();
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("e8834973-2c12-4c02-9c94-d09baa7e8d7f")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
