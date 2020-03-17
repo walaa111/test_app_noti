583af5a40047cb060a6ff9a923974e515c98a67d
walaa111 <walaagamil18@gmail.com>
2020-03-17 17:27:35 +0200

    edit

--- test/www/js/index.js
+++ test/www/js/index.js
@@ -28,6 +28,26 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        // Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("e8834973-2c12-4c02-9c94-d09baa7e8d7f")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
     },
 
     // Update DOM on a Received Event
@@ -44,20 +64,3 @@ var app = {
 };
 
 app.initialize();
-
-// Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
-
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("e8834973-2c12-4c02-9c94-d09baa7e8d7f")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-}, false);
